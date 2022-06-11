b66e4b5b6b5ceba761648fb005f4bb065524dbf8
liliya05 <liliya.uzunova05@gmail.com>
2022-06-11 11:45:08 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void findSecondMax() {
+        int[] numbs = {45, 56, 32, 47, 86, 98, 35, 46, 75, 73, 56};
+        int max = numbs[0];
+        for (int i = 0; i < numbs.length; i++) {
+            if (max < numbs[i]) {
+                max = numbs[i];
+            }
+        }
+        System.out.println(max);
+        int max2 = numbs[0];
+        for(int i = 0; i < numbs.length; i++) {
+            if(max2 < numbs[i] && max > numbs[i]) {
+                max2 = numbs[i];
+            }
+        }
+        System.out.println(max2);
+    }
+    public static void main(String[] args) {
+        findSecondMax();
+    }
+}
